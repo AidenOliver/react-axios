539925336c48dde7d7faf1c9d15d36c596b9bc34
Aiden Oliver <31047390+AidenOliver@users.noreply.github.com>
2020-04-23 13:52:51 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import './App.css';
 
 function App() {
   const [repositories, setRepositories] = useState([JSON.stringify()]);
+  const [link, setLink] = useState([JSON.stringify()]);
 
   useEffect(() => {
     getRepos();
+    getLink();
   }, []);
 
   const getRepos = async() => {
     const response = await fetch(`https://api.github.com/users/AidenOliver/repos`);
     const data = await response.json();
-    const first_name = data[0].name;
-    setRepositories(first_name);
-    console.log(first_name);
+    const repo_name = data[0].name;
+    
+    setRepositories(repo_name);
+    console.log(repo_name);
   }
 
+  const getLink = async() => {
+    const link_response = await fetch(`https://api.github.com/users/AidenOliver/repos`);
+    const link_data = await link_response.json();
+    const link_url = link_data[0].html_url;
+    
+    setLink(link_url);
+    console.log(link_url);
+  }
 
   return (
     <div className="App">
       {repositories}
+      <hr />
+      {link}
     </div>
   );
 }
